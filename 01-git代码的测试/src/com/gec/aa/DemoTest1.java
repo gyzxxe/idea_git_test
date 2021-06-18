33fb8b5c0e1b54a2b0f8670712ba0aae49814126
xiaoming <xiaoming@qq.com>
2021-06-18 17:22:26 +0800

    idea中第一次提交git

---
+++ 01-git代码的测试/src/com/gec/aa/DemoTest1.java
@@ -0,0 +1,9 @@
+package com.gec.aa;
+
+public class DemoTest1 {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+    }
+}
