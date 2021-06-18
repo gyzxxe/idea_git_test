45bf036f85040cf6c89897b6ba2f352e17a0f94f
gyzxxe <86064370+gyzxxe@users.noreply.github.com>
2021-06-18 20:55:40 +0800

    Update DemoTest1.java

--- 01-git代码的测试/src/com/gec/aa/DemoTest1.java
+++ 01-git代码的测试/src/com/gec/aa/DemoTest1.java
@@ -2,6 +2,11 @@ package com.gec.aa;
 
 public class DemoTest1 {
 
+    //这是在GitHub上添加的方法
+    public static void bb(){
+         System.out.println(666);
+    }
+    
     public static void aa(){
         System.out.println(666);
     }
