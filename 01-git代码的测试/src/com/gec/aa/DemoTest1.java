042d17c15c11e028651fc8a1ecbcbf0070f63e61
xiaoming <xiaoming@qq.com>
2021-06-18 20:29:03 +0800

    idea中第二次提交git

--- 01-git代码的测试/src/com/gec/aa/DemoTest1.java
+++ 01-git代码的测试/src/com/gec/aa/DemoTest1.java
@@ -1,6 +1,11 @@
 package com.gec.aa;
 
 public class DemoTest1 {
+
+    public static void aa(){
+        System.out.println(666);
+    }
+
     public static void main(String[] args) {
         System.out.println("aaaaaaaa");
         System.out.println("aaaaaaaa");
